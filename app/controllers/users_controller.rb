213672e1a4788cd5404b1db1f1cbf51f67a85e86
tmatsu12 <tmatsunaga65@gmail.com>
2021-08-16 11:29:54 +0000

    [Update]bookers2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,26 @@ class UsersController < ApplicationController
   end
 
   def index
+    @user = current_user
+    @users = User.all
+    @book = Book.new
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(current_user)
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+
+
 end
