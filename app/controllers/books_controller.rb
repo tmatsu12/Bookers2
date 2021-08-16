f5c7fc298f6f160be8bbd443b8305c5d5dd3c321
tmatsu12 <tmatsunaga65@gmail.com>
2021-08-16 06:27:37 +0000

    [Update]bookers2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,16 +4,14 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
-  def new
-    @book = Book.new
-  end
+
 
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
     # redirect_to book_path(@book.id)
-    redirect_to user_path(current_user.id)
+    redirect_to book_path(@book.id)
   end
 
   private
